a67cbe604aafba2f58c084d804feeed274823c49
Dunemask <dunemask@gmail.com>
2021-03-06 09:22:56 -0700

    CSS Fix for the upload button

--- views/pages/Upload.jsx
+++ views/pages/Upload.jsx
@@ -17,18 +17,16 @@ module.exports = class Upload extends Page {
         {this.BuildPage(
           <div className="upload-files-content">
             <div id="file-drop-area">
-              <form className="upload-form">
-                <input
-                  type="file"
-                  id="file-dropzone"
-                  name="file-dropzone"
-                  multiple
-                ></input>
-                <label className="button" htmlFor="file-dropzone">
-                  <span id="file-select">Select Files</span> or Drag {"n'"} Drop
-                  them here
-                </label>
-              </form>
+              <input
+                type="file"
+                id="file-dropzone"
+                name="file-dropzone"
+                multiple
+              ></input>
+              <label className="upload-button" htmlFor="file-dropzone">
+                <span id="file-select">Select Files</span> or Drag {"n'"} Drop
+                them here
+              </label>
             </div>
             <UploadDialog></UploadDialog>
           </div>
